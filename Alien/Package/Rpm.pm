1d5b6b9a90f89f762823581cc0cbd8558645d449
joey <joey@a4a2c43b-8ac3-0310-8836-e0e880c912e2>
2004-08-02 00:53:46 +0000

    releasing version 8.46

--- Alien/Package/Rpm.pm
+++ Alien/Package/Rpm.pm
@@ -284,6 +284,7 @@ sub prep {
 	print OUT "\n";
 	print OUT "\%define _rpmdir ../\n"; # write rpm to current directory
 	print OUT "\%define _rpmfilename %%{NAME}-%%{VERSION}-%%{RELEASE}.%%{ARCH}.rpm\n";
+	print OUT "\%define _unpackaged_files_terminate_build 0\n"; # work on SuSE
 	print OUT "\n";
 	if ($this->usescripts) {
 		if ($this->preinst) {
